9a925a0724fcfc8ae311251398199c8c0ad73535
Davide Bellone <davide.bellone>
2021-07-19 10:52:39 +0200

    Add project files.

---
+++ DecoratorPatternWithScrutor/Controllers/RssInfoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DecoratorPatternWithScrutor.Controllers
+{
+    /*
+    -  Installato system.servicemodel.syndication per RSS feed
+    - Scrutor (https://www.nuget.org/packages/Scrutor/3.3.0?_src=template)
+
+     */
+
+    [ApiController]
+    [Route("[controller]")]
+    public class RssInfoController : ControllerBase
+    {
+        private readonly ILogger<RssInfoController> _logger;
+
+        public RssInfoController(ILogger<RssInfoController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
